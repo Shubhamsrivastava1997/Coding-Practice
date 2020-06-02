cddeb84ca9878c60988ce69f1ddc949c2f7d00fb
Shubhamsrivastava1997 <47738036+Shubhamsrivastava1997@users.noreply.github.com>
2020-06-02 15:05:48 +0530

    Add files via upload
    Printing number from 1 to n using concept of recursion

--- Program1.cpp
+++ Program1.cpp
@@ -5,23 +5,19 @@ int fun(int n)
 {
     if(n==0)
     return 0;
-    
     fun(n-1);
     cout<<n<<" ";
     return 0;
 }
-
 
 int main() {
     int T,n;
 	cin>>T;
 	while(T--)
 	{
-	
 	    cin>>n;
 	    fun(n);
 	    cout<<endl;
-
 	}
 	return 0;
 }
