471b10dd0bf16a8bd8223d0fca10209f56655c80
Shubhamsrivastava1997 <47738036+Shubhamsrivastava1997@users.noreply.github.com>
2020-06-02 15:01:48 +0530

    Add files via upload

---
+++ Program1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int fun(int n)
+{
+    if(n==0)
+    return 0;
+    
+    fun(n-1);
+    cout<<n<<" ";
+    return 0;
+}
+
+
+int main() {
+    int T,n;
+	cin>>T;
+	while(T--)
+	{
+	
+	    cin>>n;
+	    fun(n);
+	    cout<<endl;
+
+	}
+	return 0;
+}
